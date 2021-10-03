e7f865ee4d88a58ad75a102b549d0c0a3a755d93
Tom <tomanagle@gmail.com>
2021-10-03 20:24:31 +1000

    Update user.controller.ts

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -10,7 +10,7 @@ export async function createUserHandler(
 ) {
   try {
     const user = await createUser(req.body);
-    return res.send(omit(user.toJSON(), "password"));
+    return res.send(user);
   } catch (e: any) {
     logger.error(e);
     return res.status(409).send(e.message);
