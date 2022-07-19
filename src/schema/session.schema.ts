8d1487474f54e49249411ea0ed5bfb0479dec4be
Ismail <i180516@nu.edu.pk>
2022-07-19 23:23:14 +0500

    feat: login a user docs

--- src/schema/session.schema.ts
+++ src/schema/session.schema.ts
@@ -1,12 +1,36 @@
-import { object, string } from "zod";
+import { object, string } from 'zod'
 
+/**
+ * @openapi
+ * components:
+ *  schemas:
+ *    CreateSessionInput:
+ *      type: object
+ *      required:
+ *        - email
+ *        - password
+ *      properties:
+ *        email:
+ *          types: string
+ *          default: jane.doe@example.com
+ *        password:
+ *          types: string
+ *          default: stringPassword123
+ *    CreateSessionResponse:
+ *      type: object
+ *      properties:
+ *        accessToken:
+ *          types: string
+ *        refreshToken:
+ *          types: string
+ */
 export const createSessionSchema = object({
   body: object({
     email: string({
-      required_error: "Email is required",
+      required_error: 'Email is required',
     }),
     password: string({
-      required_error: "Password is required",
+      required_error: 'Password is required',
     }),
   }),
-});
+})
