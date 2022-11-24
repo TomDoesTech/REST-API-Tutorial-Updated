b4df7297e988026dac1148c9e0050723e205be15
ibukunfolay <70657237+ibukunfolay@users.noreply.github.com>
2022-11-24 02:36:25 +0100

    Update user.service.ts

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -3,12 +3,14 @@ import { omit } from "lodash";
 import UserModel, { UserDocument, UserInput } from "../models/user.model";
 
 export async function createUser(input: UserInput) {
-  try {
-    const user = await UserModel.create(input);
+  const { email } = input;
+  const existingUser = await userModel.findOne({ email });
 
-    return omit(user.toJSON(), "password");
-  } catch (e: any) {
-    throw new Error(e);
+  if (!existingUser) {
+    const user = await userModel.create(input);
+    return omit(user.toJSON(), 'password');
+  } else {
+    throw new Error('email already in use');
   }
 }
 
