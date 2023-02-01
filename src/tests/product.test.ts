f98ad682b9b57315237b47ac2c1018d76f3c277d
StephenWilkens <stephenwilkens@gmail.com>
2023-01-31 18:30:45 -0700

    Setup test script and test folder with initial describe blocks for products

---
+++ src/tests/product.test.ts
@@ -0,0 +1,9 @@
+describe("product", () => {
+  describe("get product route", () => {
+    describe("given the product does not exist", () => {
+      it("should return a 404", () => {
+        expect(true).toBe(true);
+      });
+    });
+  });
+});
