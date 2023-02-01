7b2755a215093dcce843150bc30a7dc158fc6679
StephenWilkens <stephenwilkens@gmail.com>
2023-01-31 18:19:57 -0700

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ["**/**/*.test.ts"],
+  verbose: true,
+  forceExit: true, 
+  // clearMocks: true,
+};
