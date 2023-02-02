8ceafb15ff57f96fc3700304bbf20dafce1447eb
StephenWilkens <stephenwilkens@gmail.com>
2023-02-02 16:47:35 -0700

    Fix open handle

--- src/tests/product.test.ts
+++ src/tests/product.test.ts
@@ -1,8 +1,13 @@
+import supertest = require("supertest");
+import {app} from '../app'
+
 describe("product", () => {
   describe("get product route", () => {
     describe("given the product does not exist", () => {
-      it("should return a 404", () => {
-        expect(true).toBe(true);
+      it("should return a 404", async () => {
+        const productId = 'product-123'
+
+        await supertest(app).get(`/api/products/${productId}`).expect(404);
       });
     });
   });
