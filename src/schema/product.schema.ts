10bf133b2a82627ad5f6d42bf6faa69fe61addf8
aka-rabbi-inv <87089810+aka-rabbi-inv@users.noreply.github.com>
2023-04-03 10:15:41 +0600

    feat: add missing specs

--- src/schema/product.schema.ts
+++ src/schema/product.schema.ts
@@ -14,12 +14,40 @@ import { object, number, string, TypeOf } from "zod";
  *       properties:
  *         title:
  *           type: string
+ *           default: "Canon EOS 1500D DSLR Camera with 18-55mm Lens"
+ *         description:
+ *           type: string
+ *           default: "Designed for first-time DSLR owners who want impressive results straight out of the box, capture those magic moments no matter your level with the EOS 1500D. With easy to use automatic shooting modes, large 24.1 MP sensor, Canon Camera Connect app integration and built-in feature guide, EOS 1500D is always ready to go."
+ *         price:
+ *           type: number
+ *           default: 879.99
+ *         image:
+ *           type: string
+ *           default: "https://i.imgur.com/QlRphfQ.jpg"
+ *     productResponse:
+ *       type: object
+ *       properties:
+ *         user:
+ *           type: string
+ *         _id:
+ *           type: string
+ *         title:
+ *           type: string
  *         description:
  *           type: string
  *         price:
  *           type: number
  *         image:
  *           type: string
+ *         productId:
+ *           type: string
+ *         createdAt:
+ *           type: string
+ *         updatedAt:
+ *           type: string
+ *         __v:
+ *           type: number
+ *
  */
 
 const payload = {
